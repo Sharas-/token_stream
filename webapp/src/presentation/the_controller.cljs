b2a52efef3c0e1bcc3604abf705b7a4dc534ce30
sharas <apkrovos@gmail.com>
2020-03-23 19:45:32 +0000

    unified/formalized app step execution in presentation controller

--- webapp/src/presentation/the_controller.cljs
+++ webapp/src/presentation/the_controller.cljs
@@ -18,23 +18,16 @@
     [:label {:for "token-address"} "token address"]
     [:input.token-address {:type "text"}]])
 
-;(defn execute[step-name step sucess-content success-status]
-  ;(render state-name step-name)
-  ;(-> (step)
-      ;(.then (fn[result]
-               ;(render state-name (success-status result))
-               ;(render content [sucess-content])))
-      ;(.catch #(render content (str "error: " %)))))
+(defn execute[step-name step sucess-content success-status]
+  (render state-name step-name)
+  (-> (step)
+      (.then (fn[result]
+               (render state-name (success-status result))
+               (render content [sucess-content])))
+      (.catch #(render content (str "error: " %)))))
 
 (defn ^:export main[]
-  (render state-name "Connecting to ethereum")
   (if (eth-provider/not-installed?)
     (render state-name "Ethereum provider is not installed. Please install it to continue.")
     ;else
-    ;(execute "Connecting to ethereum provider", eth-provider/connect, create-token-stream, #(str "connected to account: " %))))
-    (-> (eth-provider/connect)
-        (.then  (fn[account]
-                  (render state-name (str "connected to account: " account))
-                  (render content create-token-stream)))
-        (.catch (fn[error]
-                  (render state-name (str "error: " error)))))))
+    (execute "Connecting to ethereum provider", eth-provider/connect, create-token-stream, #(str "connected to account: " %))))
