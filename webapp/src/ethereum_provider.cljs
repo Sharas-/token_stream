d0cf4ec8c4bf120e7aa4dbaee93ade4808994659
sharas <apkrovos@gmail.com>
2020-03-23 19:41:14 +0000

    created skeleton for gui representation of app states, errors and content

--- webapp/src/ethereum_provider.cljs
+++ webapp/src/ethereum_provider.cljs
@@ -1,12 +1,22 @@
 (ns ethereum-provider
   (:require [ethers]))
 
-(def mmask (.-ethereum js/window))
+(def ethereum-client (.-ethereum js/window))
+(def current-account)
 
-(defn not-installed?[] 
-  (nil? mmask))
+(def erc20-method{ :balanceOf "[{ \"constant\":true,\"inputs\":[{\"name\":\"_owner\",\"type\":\"address\"}],\"name\":\"balanceOf\",\"outputs\":[{\"name\":\"balance\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]"})
+
+(defn not-installed?[]
+  (or (nil? ethereum-client) 
+      (not (.-isMetaMask ethereum-client))))
 
 (defn connect[]
-    (if (not-installed?)
-      (throw "MetaMask is not installed")
-      (.send mmask "eth_requestAccounts")))
+  (-> (.send ethereum-client "eth_requestAccounts")
+      (.then #(set! current-account (get (.-result %) 0)))
+      (.catch #(throw (.-message %)))))
+
+(defn get-token-balance[token-address]
+  (let [provider (new ethers/providers.Web3Provider ethereum-client)
+        contract (new ethers/Contract token-address (:balanceOf erc20-method) provider)]
+    (.balanceOf contract current-account)))
+  
--- webapp/src/presentation/the_controller.cljs
+++ webapp/src/presentation/the_controller.cljs
@@ -2,25 +2,39 @@
   (:require [reagent.dom :as rdom]
             [ethereum-provider :as eth-provider]))
 
-(defonce dom-root (js/document.getElementById "app"))
+(def state-name (js/document.getElementById "status"))
+(def content (js/document.getElementById "main"))
 
-(defn render[content]
-  (rdom/render content dom-root))
+(defn render[container content]
+  (rdom/render content container))
 
 ;;--- HTML compontents ---
-(defn status[message]
-  [:p message])
+;(defn status [message]
+;    [:p message])
 
-(defn ethereum-connection[account]
-  [:div (str "connected to account: " account)])
+(defn create-token-stream[]
+  [:p "create new token stream"
+    [:br] 
+    [:label {:for "token-address"} "token address"]
+    [:input.token-address {:type "text"}]])
+
+;(defn execute[step-name step sucess-content success-status]
+  ;(render state-name step-name)
+  ;(-> (step)
+      ;(.then (fn[result]
+               ;(render state-name (success-status result))
+               ;(render content [sucess-content])))
+      ;(.catch #(render content (str "error: " %)))))
 
 (defn ^:export main[]
-  (render [status "Connecting to ethereum"])
+  (render state-name "Connecting to ethereum")
   (if (eth-provider/not-installed?)
-    (render [status "Ethereum provider is not installed. Please install it to continue."])
+    (render state-name "Ethereum provider is not installed. Please install it to continue.")
     ;else
+    ;(execute "Connecting to ethereum provider", eth-provider/connect, create-token-stream, #(str "connected to account: " %))))
     (-> (eth-provider/connect)
-        (.then #(let [current-account (.-result %)]
-                     (render [ethereum-connection current-account])))
-        (.catch #(render [status (str "Error connecting to ethereum: " %)])))))
-
+        (.then  (fn[account]
+                  (render state-name (str "connected to account: " account))
+                  (render content create-token-stream)))
+        (.catch (fn[error]
+                  (render state-name (str "error: " error)))))))
