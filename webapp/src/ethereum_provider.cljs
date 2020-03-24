92e980df826ab783dce2c1a28d0130bb5014c711
sharas <apkrovos@gmail.com>
2020-03-24 17:11:44 +0000

    Attaching to account change event of metamask and rerendering content

--- webapp/src/ethereum_provider.cljs
+++ webapp/src/ethereum_provider.cljs
@@ -2,7 +2,6 @@
   (:require [ethers]))
 
 (def ethereum-client (.-ethereum js/window))
-(def current-account)
 
 (def erc20-method{ :balanceOf "[{ \"constant\":true,\"inputs\":[{\"name\":\"_owner\",\"type\":\"address\"}],\"name\":\"balanceOf\",\"outputs\":[{\"name\":\"balance\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]"})
 
@@ -10,13 +9,13 @@
   (or (nil? ethereum-client) 
       (not (.-isMetaMask ethereum-client))))
 
-(defn connect[]
+(defn connect[on-accounts-changed]
   (-> (.send ethereum-client "eth_requestAccounts")
-      (.then #(set! current-account (get (.-result %) 0)))
+      (.then (.on ethereum-client "accountsChanged" on-accounts-changed))
       (.catch #(throw (.-message %)))))
 
-(defn get-token-balance[token-address]
+(defn get-token-balance[account, token-address]
   (let [provider (new ethers/providers.Web3Provider ethereum-client)
         contract (new ethers/Contract token-address (:balanceOf erc20-method) provider)]
-    (.balanceOf contract current-account)))
+    (.balanceOf contract account)))
   
--- webapp/src/presentation/the_controller.cljs
+++ webapp/src/presentation/the_controller.cljs
@@ -2,7 +2,7 @@
   (:require [reagent.dom :as rdom]
             [ethereum-provider :as eth-provider]))
 
-(def state-name (js/document.getElementById "status"))
+(def provider-state (js/document.getElementById "status"))
 (def content (js/document.getElementById "main"))
 
 (defn render[container content]
@@ -12,22 +12,25 @@
 ;(defn status [message]
 ;    [:p message])
 
-(defn create-token-stream[]
+(defn create-token-stream[account]
   [:p "create new token stream"
     [:br] 
     [:label {:for "token-address"} "token address"]
     [:input.token-address {:type "text"}]])
 
-(defn execute[step-name step sucess-content success-status]
-  (render state-name step-name)
-  (-> (step)
-      (.then (fn[result]
-               (render state-name (success-status result))
-               (render content [sucess-content])))
-      (.catch #(render content (str "error: " %)))))
+(defn on-accounts-changed[accounts]
+  (render content "")
+  (if (empty? accounts)
+    (render provider-state "Connect to ethereum provider to continue")
+    ;else
+    (let [account (get accounts 0)]
+      (render provider-state (str "Connected to account: " account))
+      (render content [create-token-stream account]))))
 
 (defn ^:export main[]
   (if (eth-provider/not-installed?)
-    (render state-name "Ethereum provider is not installed. Please install it to continue.")
+    (render provider-state "Ethereum provider is not installed. Please install it to continue.")
     ;else
-    (execute "Connecting to ethereum provider", eth-provider/connect, create-token-stream, #(str "connected to account: " %))))
+    (-> (eth-provider/connect on-accounts-changed)
+        (.catch #(render provider-state (str "Error: " %))))))
+
